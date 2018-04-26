ed8a2ce9b9cd9202c14f11091a7c1fe0e495f207
julialeshheva <38296466+julialeshheva@users.noreply.github.com>
2018-04-26 20:41:09 +0300

    Рекомендуемая сумма платежа

--- BankingSystem/BankingSystem/BusinessLogic/CreditRegulator.cs
+++ BankingSystem/BankingSystem/BusinessLogic/CreditRegulator.cs
@@ -8,6 +8,24 @@ namespace BankingSystem.BusinessLogic
 {
     class CreditRegulator
     {
-        
+
+        public int recommendedSumOfDebt(bank_account account, credit_type typeCredit, credit credit) // рекомендуемая сумма долга
+        {
+            int recommendedSum = 0;
+            int sumOfDebt = typeCredit.Sum;
+            double interestRate = typeCredit.Interest_rate;
+            int timeOfcredit = 6; //Convert.ToInt32((credit.Expiry_date - credit.Opening_date).TotalDays / 30); Не знаю как посчитать количество месяцев
+            int monthPayment = sumOfDebt / timeOfcredit; // ежемесяный платеж без учта процентов
+            if (credit.Paid_sum == 0 || credit.Paid_sum == null)
+            {
+                return recommendedSum = Convert.ToInt32((sumOfDebt * interestRate / 100) / 12 + monthPayment);
+            }
+            else
+            {
+                sumOfDebt = typeCredit.Sum - credit.Paid_sum.Value;
+                return recommendedSum = Convert.ToInt32((sumOfDebt * interestRate / 100) / 12 + monthPayment);
+            }
+        }
+
     }
 }
