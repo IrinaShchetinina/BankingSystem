092f5e292eaca01c65173938456f03fa8c405a5f
julialeshheva <38296466+julialeshheva@users.noreply.github.com>
2018-04-27 09:08:57 +0300

    Метод в класе CreditRegulator

--- BankingSystem/BankingSystem/BusinessLogic/CreditRegulator.cs
+++ BankingSystem/BankingSystem/BusinessLogic/CreditRegulator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BankingSystem.BusinessLogic
 {
     class CreditRegulator
     {
 
+       private int recommendedSumForPay;
         public int recommendedSumOfDebt(bank_account account, credit_type typeCredit, credit credit) // рекомендуемая сумма долга
         {
             int recommendedSum = 0;
@@ -18,14 +20,39 @@ namespace BankingSystem.BusinessLogic
             int monthPayment = sumOfDebt / timeOfcredit; // ежемесяный платеж без учта процентов
             if (credit.Paid_sum == 0 || credit.Paid_sum == null)
             {
-                return recommendedSum = Convert.ToInt32((sumOfDebt * interestRate / 100) / 12 + monthPayment);
+                recommendedSum = Convert.ToInt32((sumOfDebt * interestRate / 100) / 12 + monthPayment);
+                recommendedSumForPay = recommendedSum;
+                return recommendedSumForPay;
             }
             else
             {
                 sumOfDebt = typeCredit.Sum - credit.Paid_sum.Value;
-                return recommendedSum = Convert.ToInt32((sumOfDebt * interestRate / 100) / 12 + monthPayment);
+                recommendedSum = Convert.ToInt32((sumOfDebt * interestRate / 100) / 12 + monthPayment);
+                recommendedSumForPay = recommendedSum;
+                return recommendedSumForPay;
             }
         }
+
+        public void PayForCredit(bank_account sum, int sumForPay, credit paid)
+        {
+            int sumTotal = Convert.ToInt32(sum.Sum);
+            int paidSum = Convert.ToInt32(paid.Paid_sum);
+            if(sumTotal<sumForPay)
+            {
+                MessageBox.Show("You do not have enough money to pay your credit. Please selet another account");
+            }
+            else
+            {
+                if (sumForPay<recommendedSumForPay)
+                {
+                    MessageBox.Show("You can not pay less than the recommended amount. Please enter another sum for paid");
+                }
+                sum.Sum = sumTotal - sumForPay;
+                paid.Paid_sum = paidSum + sumForPay;
+            }
+
+        }
+
 
     }
 }
